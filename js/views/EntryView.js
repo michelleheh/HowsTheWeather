3c78c267fe5fcc1623c57322f23d442dfde5b080
Xiaoyu He <xiaoyuhe@Jonathans-MacBook-Air-2.local>
2016-03-23 21:12:47 -0700

    add comments

--- js/views/EntryView.js
+++ js/views/EntryView.js
@@ -1,7 +1,7 @@
 var EntryView = Backbone.View.extend({
 
-  className: 'entry',
-
+  className: 'entry', // see class to 'list' so that css can style it
+  // not provided by Backbone but is a convention to set it to 'template'; see line 20 and underscore documents where _.templete returns a function;
   template: _.template('<p>It is currently <%= weather %><%= unit %> in <%= city %>.</p>'),
 
   events: {
@@ -16,7 +16,7 @@ var EntryView = Backbone.View.extend({
   },
 
   render: function() {
-
+    // use template to replace tokens
     var entry = this.template({
       weather: this.model.get('weather'),
       unit: this.model.get('unit'),
--- js/views/ListView.js
+++ js/views/ListView.js
@@ -1,10 +1,11 @@
 var ListView = Backbone.View.extend({
   // if el is not set, default to <div>
-  id: 'list', // see id to 'list' so that css can work
+  id: 'list', // see id to 'list' so that css can style it
 
   initialize: function() {
+    // listenTo is a event provided by BackBone
+    // this.collection.on('add', this.render, this); // also works 
     this.listenTo(this.collection, 'add', this.render);
-    //this.collection.on('add', this.render, this);
   },
 
   render: function() {
